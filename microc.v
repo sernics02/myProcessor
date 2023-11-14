eba4458a01a2f8a8f4fa78c2d2ddf34fd3b1985b
sernics <alu0101389936@ull.edu.es>
2023-11-14 15:45:32 +0000

    Datos principales de la practica

---
+++ microc.v
@@ -0,0 +1,5 @@
+module microc(output wire [5:0] Opcode, output wire zero, input wire clk, reset, s_inc, s_inm, we, wez, input wire [2:0] ALUOp);
+//Microcontrolador sin memoria de datos de un solo ciclo
+
+
+endmodule
