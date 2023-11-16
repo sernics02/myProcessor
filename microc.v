e5ab82b5f04ba7a3b59bf98c1746730816726023
sernics <alu0101389936@ull.edu.es>
2023-11-16 16:49:04 +0000

    Pequeños cambios en el PC

--- microc.v
+++ microc.v
@@ -1,27 +1,14 @@
 module microc(output wire [5:0] Opcode, output wire zero, input wire clk, reset, s_inc, s_inm, we, wez, input wire [2:0] ALUOp);
     //Microcontrolador sin memoria de datos de un solo ciclo
 
-    // Left side of the microcontroller
-    reg[9:0] ZERO = 10'b0000000000;
-    reg[9:0] ONE = 10'b0000000001;
-    wire[9:0] PC, newPC, outPCSum;
-    wire[15:0] Instruction;
-    wire[9:0] DirSalto;
-    // module mux2 #(parameter WIDTH = 8)
-    //          (output wire [WIDTH-1:0] Y,      // Salida del multiplexor
-    //           input  wire [WIDTH-1:0] D0, D1, // Entradas del multiplexor
-    //           input  wire             s);     // Selección
+    wire[15:0] instruction;
+    wire[9:0] new_pc, actual_pc, jump_addr, add_value, value_one;
+    wire[7:0] inmediate, WD3, RD1, RD2, out_mux_inm;
+    wire[3:0] RA1, RA2, WA3;
 
-    sum sumPC(outPCSum, PC, ONE);
-    assign Opcode = [15:10] Instruction;
-    assign DirSalto = [9:0] Instruction;
-    mux muxPC(newPC, DirSalto, outPCSum, s_inc);
-
-    // Right side of the microcontroller
-    wire[3:0] RA1, WA3, RA2;
-    wire[7:0] Inm;
-    wire[3:0] outMuxRA1WA3;
-    wire[7:0] RD1, RD2;
-    mux muxRegistro(outMuxRA1WA3, RA1, WA3, s_inm);
-    regfile bancoRegistros(RD1, RD2);
-endmodule
+    // Program Counter Changes
+    assign value_one = 10'b0000000001;
+    sum increment_pc(add_value, value_one, actual_pc);
+    mux2 mux_pc(new_pc, jump_addr, add_value, s_inc);
+    
+endmodule 
