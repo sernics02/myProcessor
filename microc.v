ff4af0cd17cc695962cf77f937b7be9435c163da
sernics <alu0101389936@ull.edu.es>
2023-11-16 17:10:15 +0000

    Alu done

--- microc.v
+++ microc.v
@@ -3,7 +3,7 @@ module microc(output wire [5:0] Opcode, output wire zero, input wire clk, reset,
 
     wire[15:0] instruction;
     wire[9:0] new_pc, actual_pc, jump_addr, add_value, value_one;
-    wire[7:0] inmediate, WD3, RD1, RD2, out_mux_inm;
+    wire[7:0] inmediate, WD3, RD1, RD2;
     wire[3:0] RA1, RA2, WA3;
 
     // Program Counter Changes
@@ -23,4 +23,17 @@ module microc(output wire [5:0] Opcode, output wire zero, input wire clk, reset,
     assign RA2 = instruction[7:4];
     assign WA3 = instruction[3:0];
     assign inmediate = instruction[11:4];
+
+    // Selection first register
+    wire[3:0] out_mux_reg;
+    mux2 outMuxReg(out_mux_reg, RA1, WA3, s_inm);
+
+    // Register bank
+    regfile regfile(RD1, RD2, clk, we, wez, out_mux_reg, RA2, WA3, WD3);
+
+    // ALU
+    wire[7:0] out_alu_mux;
+    wire zero;
+    mux mux_alu(out_alu_mux, RD2, inmediate, s_inm);
+    alu estrellita(WD3, zero, RD1, out_alu_mux, ALUOp);
 endmodule 
