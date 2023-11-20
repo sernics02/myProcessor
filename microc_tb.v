a4d438a8bee5ca429174450476ec4bde98ea35e7
sernics <alu0101389936@ull.edu.es>
2023-11-20 17:52:31 +0000

    Añadido el pdf

--- microc_tb.v
+++ microc_tb.v
@@ -1,42 +1,44 @@
 `timescale 1ns/10ps
 
 module microc_tb;
+  // Señales, salidas
   wire[5:0] Opcode;
   wire zero;
+
+  // Señales, entradas
   reg reset, clk, s_inc, s_inm, we, wez;
   reg[2:0] ALUOp;
-
+  
+  // Módulo
   microc microc_instanste(Opcode, zero, clk, reset, s_inc, s_inm, we, wez, ALUOp);
   
+  // Reloj
   always
-  begin
-    clk = 1;
-    #20;
-    clk = 0;
-    #20;
-  end
+    begin
+      clk <= 1'b1;
+      #20;
+      clk <= 1'b0;
+      #20;
+    end
 
   initial
-  begin
-    reset = 1'b1;
-    #5;
-    reset = 1'b0;
-  end
+    begin
+      reset <= 1'b1;
+      #3;
+      reset <= 1'b0;
+      #2;
+    end
 
+  // Pruebas
   initial
-  begin
-    $dumpfile("microc_tb.vcd");
-    $dumpvars;
-    // Mostrar tiempo, opcode, zero, clk, reset, s_inc, s_inm, we, wez, ALUOp
-    $monitor("%t %b %b %b %b %b %b %b %b %b", $time, Opcode, zero, clk, reset, s_inc, s_inm, we, wez, ALUOp);
-    #20
-    s_inc = 1'b1;
-    s_inm = 1'b1;
-    we = 1'b1;
-    wez = 1'b1;
-    ALUOp = 3'b000;
-    #40
-    $finish;
-  end
+    begin
+      $monitor("AluOP(%b) inc(%b) inm(%b) we(%b) wez(%b)", Opcode, s_inc, s_inm, we, wez);
+      $dumpfile("micro_tb.vcd");
+      $dumpvars;
+
+      #20;
+      #40;
+      $finish;
+    end
 
 endmodule
