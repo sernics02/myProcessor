c469caaafe3a8ce8854c9c9c7bda33bc88e729ea
sernics <alu0101389936@ull.edu.es>
2023-11-24 16:54:15 +0000

    Primera version de la uc

--- uc.v
+++ uc.v
@@ -1,3 +1,41 @@
-module uc(output wire s_inc, s_inm, we, wez, output wire[2:0] AluOp, input wire, clk, zero, input wire[5:0] Opcode);
+module uc (
+  output reg s_inc, s_inm, we, wez,
+  output reg [2:0] AluOp,
+  input wire clk, zero,
+  input wire [5:0] Opcode
+);
 
+  always @(negedge clk) begin
+    casez (Opcode)
+      // Los que empiezan por 0
+      6'b0????? : begin
+        AluOp = Opcode[4:2];
+        we = 1'b1;
+        wez = 1'b1;
+        s_inm = 1'b0;
+        s_inc = 1'b0;
+
+      end
+      // Los que empiezan por 1
+      6'b1????? : begin
+        AluOp = Opcode[4:2];
+        we = 1'b1;
+        wez = 1'b1;
+        s_inm = 1'b0;
+        s_inc = 1'b0;
+
+      end
+      6'b1100?? : begin
+        AluOp = 3'b000;
+        we = 1'b0;
+        wez = 1'b0;
+        s_inm = 1'b1;
+        case(Opcode[1:0])
+          00: s_inc = 1'b0;
+          01: s_inc = ~zero;
+          10: s_inc = zero;
+        endcase
+      end
+    endcase
+  end
 endmodule
